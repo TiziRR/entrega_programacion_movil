7387231857199ab032d9c645c9d4a1a587a406ec
Tiziano <tizianorossi508@gmail.com>
2025-11-01 19:39:29 -0300

    add adapters Product and Carousel

---
+++ app/src/main/java/com/example/entregamovil/adapters/ProductAdapter.java
@@ -0,0 +1,58 @@
+package com.example.entregamovil.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.entregamovil.R;
+import com.example.entregamovil.models.Product;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    private List<Product> productList;
+
+    public ProductAdapter(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_product, parent, false);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Product product = productList.get(position);
+        holder.tvProductName.setText(product.getName());
+        holder.ivProductImage.setImageResource(product.getImageResId());
+        holder.tvRating.setText(product.getRating() + "/" + product.getRatingCount());
+    }
+
+    @Override
+    public int getItemCount() {
+        return productList.size();
+    }
+
+    public static class ProductViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivProductImage;
+        TextView tvProductName;
+        TextView tvRating;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivProductImage = itemView.findViewById(R.id.ivProductImage);
+            tvProductName = itemView.findViewById(R.id.tvProductName);
+            tvRating = itemView.findViewById(R.id.tvRating);
+        }
+    }
+}
